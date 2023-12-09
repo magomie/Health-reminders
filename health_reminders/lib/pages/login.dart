9de9ac7d7d27c61288f0047e6b10640bae3d5687
magomie <113960532+magomie@users.noreply.github.com>
2023-12-09 17:50:26 +0700

    แก้ Overflow
    Co-Authored-By: NEO <75066153+N3O-N@users.noreply.github.com>

--- health_reminders/lib/pages/login.dart
+++ health_reminders/lib/pages/login.dart
@@ -9,131 +9,134 @@ class loginPage extends StatelessWidget {
     final TextEditingController passwordController = TextEditingController();
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
       ),
-      body: Center(
-      
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 150,),
-              Text('เข้าสู่ระบบ', style: TextStyle(
-          color: Color.fromRGBO(107, 89, 24, 1),
-          fontFamily: 'Garuda',
-          fontWeight: FontWeight.bold,
-          fontSize: 24),
-              ),
-              Column(
-                children: [
-                  SizedBox(height: 10,),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30.0 , vertical: 12.0),
-                    child: TextField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 15.0), 
-                        border: OutlineInputBorder(),
-                       focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 2.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-
-                      ),
-                    ),
-                  ),
-                  
-                 SizedBox(height: 5,),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30.0 , vertical: 12.0),
-                    child: TextField(
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 15.0), 
-                        border: OutlineInputBorder(),
-                        focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 2.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      ),
-                    ),
-                  ),    
-                ],
-              ),
-              
-              //SizedBox(height: 5,),
-               Column(
-                 children: [
-                   Row(
-                     children: [
-                      SizedBox(width: 285,),
-                       GestureDetector(
-                        onTap: () {
-                          // Handle the "Forgot Password" action
-                          print('Forgot Password tapped');
-                        },
-                       
-                        child: Text('ลืมรหัสผ่าน?', style: TextStyle(
-                            color: const Color.fromARGB(255, 43, 48, 53), // You can change the color as needed
-                            decoration: TextDecoration.underline,
-                            fontSize: 14,
-                            
-                          ),
+      body: SingleChildScrollView(
+        child: Center(
+        
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 150,),
+                Text('เข้าสู่ระบบ', style: TextStyle(
+            color: Color.fromRGBO(107, 89, 24, 1),
+            fontFamily: 'Garuda',
+            fontWeight: FontWeight.bold,
+            fontSize: 24),
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30.0 , vertical: 12.0),
+                      child: TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 15.0), 
+                          border: OutlineInputBorder(),
+                         focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey, width: 2.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+        
                         ),
                       ),
-                     ],
-                   ),
-                 ],
-               ),
-              
-              SizedBox(height: 10,),
-              ElevatedButton(
-                style: buttonlgin,
-                onPressed: () {
-                  // Handle the first button press
-                  print('Button 1 Pressed');
-                },
-                child: Text('เข้าสู่ระบบ',style:TextStyles.Tlogin),
-              ),
-
-              SizedBox(height: 10,),
-              GestureDetector(
-                        onTap: () {
-                          // Handle the "Forgot Password" action
-                          print('Forgot Password tapped');
-                        },
-                       
-                        child: Text('หรือ', style: TextStyle(
-                            color: const Color.fromARGB(255, 43, 48, 53), // You can change the color as needed
-                            fontSize: 14,
-                            
+                    ),
+                    
+                   SizedBox(height: 5,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30.0 , vertical: 12.0),
+                      child: TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 15.0), 
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey, width: 2.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        ),
+                      ),
+                    ),    
+                  ],
+                ),
+                
+                //SizedBox(height: 5,),
+                 Column(
+                   children: [
+                     Row(
+                       children: [
+                        SizedBox(width: 285,),
+                         GestureDetector(
+                          onTap: () {
+                            // Handle the "Forgot Password" action
+                            print('Forgot Password tapped');
+                          },
+                         
+                          child: Text('ลืมรหัสผ่าน?', style: TextStyle(
+                              color: const Color.fromARGB(255, 43, 48, 53), // You can change the color as needed
+                              decoration: TextDecoration.underline,
+                              fontSize: 14,
+                              
+                            ),
                           ),
                         ),
+                       ],
+                     ),
+                   ],
                  ),
-
-              SizedBox(height: 10,),
-              GestureDetector(
-                        onTap: () {
-                          // Handle the "Forgot Password" action
-                          print('Forgot Password tapped');
-                        },
-                       
-                        child: Text('สมัครสมาชิก', style: TextStyle(
-                            color: const Color.fromARGB(255, 43, 48, 53), // You can change the color as needed
-                            fontSize: 14,
+                
+                SizedBox(height: 10,),
+                ElevatedButton(
+                  style: buttonlgin,
+                  onPressed: () {
+                    // Handle the first button press
+                    print('Button 1 Pressed');
+                  },
+                  child: Text('เข้าสู่ระบบ',style:TextStyles.Tlogin),
+                ),
+        
+                SizedBox(height: 10,),
+                GestureDetector(
+                          onTap: () {
+                            // Handle the "Forgot Password" action
+                            print('Forgot Password tapped');
+                          },
+                         
+                          child: Text('หรือ', style: TextStyle(
+                              color: const Color.fromARGB(255, 43, 48, 53), // You can change the color as needed
+                              fontSize: 14,
+                              
+                            ),
+                          ),
+                   ),
+        
+                SizedBox(height: 10,),
+                GestureDetector(
+                          onTap: () {
+                            // Handle the "Forgot Password" action
+                            print('Forgot Password tapped');
+                          },
+                         
+                          child: Text('สมัครสมาชิก', style: TextStyle(
+                              color: const Color.fromARGB(255, 43, 48, 53), // You can change the color as needed
+                              fontSize: 14,
+                          ),
                         ),
                       ),
-                    ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
