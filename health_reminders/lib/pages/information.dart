dafb51c2d52b26aad6f9685bc2f86b96ff1c2b7d
NEO <75066153+N3O-N@users.noreply.github.com>
2023-12-12 21:34:48 +0700

    แก้ imformation page
    แก้ส่วนที่กอมไม่เข้าใจแล้วนะ

--- health_reminders/lib/pages/information.dart
+++ health_reminders/lib/pages/information.dart
@@ -11,14 +11,18 @@ class informationPage extends StatefulWidget {
 
 class _informationPageState extends State<informationPage> {
   String? valueselect;
-  List listitem = [ "ไม่มีการออกกำลังกาย", 
-                    "ออกกำลังกายเล็กน้อยอาทิตย์ละ 1-3 วัน", 
-                    "ออกกำลังกายปานกลางอาทิตย์ละ 3-5 วัน", 
-                    "ออกกำลังกายอย่างหนักอาทิตย์ละ 6-7 วัน",
-                    "ออกกำลังกายอย่างหนักทุกวัน" ];
+  List listitem = [
+    "ไม่มีการออกกำลังกาย",
+    "ออกกำลังกายเล็กน้อยอาทิตย์ละ 1-3 วัน",
+    "ออกกำลังกายปานกลางอาทิตย์ละ 3-5 วัน",
+    "ออกกำลังกายอย่างหนักอาทิตย์ละ 6-7 วัน",
+    "ออกกำลังกายอย่างหนักทุกวัน"
+  ];
 
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+  final TextEditingController heightController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -42,35 +46,56 @@ class _informationPageState extends State<informationPage> {
         child: Center(
           child: Container(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 SizedBox(
-                  height: 120,
+                  height: 50,
                 ),
-                Text(
-                  'ใส่รูปอย่างไร',
-                  style: TextStyle(
-                      color: Color.fromRGBO(107, 89, 24, 1),
-                      fontFamily: 'Garuda',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      children: [
+                        CircleAvatar(
+                          radius: 64,
+                          backgroundColor: yellow,
+                        ),
+                        Positioned(
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.photo_outlined,
+                              size: 50,
+                            ),
+                          ),
+                          top: 64 - 32,
+                          left: 64 - 32,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
                 Column(
                   children: [
                     SizedBox(
                       height: 10,
                     ),
+                    Text(
+                      'กรุณากรอกข้อมูลทั้งหมด',
+                      textAlign: TextAlign.left,
+                    ),
                     Padding(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 30.0, vertical: 12.0),
                       child: TextField(
-                        controller: emailController,
+                        controller: nameController,
                         decoration: InputDecoration(
                           labelText: 'ชื่อ',
                           labelStyle: TextStyle(
-                                  color: brown, // สีของ labelText
-                                  fontSize: 16,
-                                  fontFamily: 'Garuda' // ขนาด font ของ labelText
-                                ),
+                              color: brown, // สีของ labelText
+                              fontSize: 16,
+                              fontFamily: 'Garuda' // ขนาด font ของ labelText
+                              ),
                           contentPadding: EdgeInsets.symmetric(
                               vertical: 12.0, horizontal: 15.0),
                           border: OutlineInputBorder(),
@@ -94,14 +119,14 @@ class _informationPageState extends State<informationPage> {
                       padding: const EdgeInsets.symmetric(
                           horizontal: 30.0, vertical: 12.0),
                       child: TextField(
-                        controller: passwordController,
+                        controller: ageController,
                         decoration: InputDecoration(
                           labelText: 'อายุ',
                           labelStyle: TextStyle(
-                                  color: brown, // สีของ labelText
-                                  fontSize: 16,
-                                  fontFamily: 'Garuda' // ขนาด font ของ labelText
-                                ),
+                              color: brown, // สีของ labelText
+                              fontSize: 16,
+                              fontFamily: 'Garuda' // ขนาด font ของ labelText
+                              ),
                           contentPadding: EdgeInsets.symmetric(
                               vertical: 12.0, horizontal: 15.0),
                           border: OutlineInputBorder(),
@@ -117,37 +142,86 @@ class _informationPageState extends State<informationPage> {
                           ),
                         ),
                       ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment
+                          .spaceBetween, // Adjust the alignment as needed
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              right: 5.0,
+                              left: 30,
+                              top: 12,
+                              bottom: 12,
+                            ),
+                            child: TextField(
+                              controller: weightController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText: 'น้ำหนัก (กก.)',
+                                labelStyle: TextStyle(
+                                  color: Colors.brown,
+                                  fontSize: 16,
+                                  fontFamily: 'Garuda',
+                                ),
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 12.0, horizontal: 15.0),
+                                border: OutlineInputBorder(),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey, width: 2.0),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey, width: 1.0),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              right: 30.0,
+                              left: 5,
+                              top: 12,
+                              bottom: 12,
+                            ),
+                            child: TextField(
+                              controller: heightController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText: 'ส่วนสูง (ซม.) ',
+                                labelStyle: TextStyle(
+                                  color: Colors.brown,
+                                  fontSize: 16,
+                                  fontFamily: 'Garuda',
+                                ),
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 12.0, horizontal: 15.0),
+                                border: OutlineInputBorder(),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey, width: 2.0),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey, width: 1.0),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                     SizedBox(
                       height: 1,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 30.0, vertical: 12.0),
-                      child: TextField(
-                        controller: passwordController,
-                        decoration: InputDecoration(
-                          labelText: 'confirm password',
-                          labelStyle: TextStyle(
-                                  color: brown, // สีของ labelText
-                                  fontSize: 16,
-                                  fontFamily: 'Garuda' // ขนาด font ของ labelText
-                                ),
-                          contentPadding: EdgeInsets.symmetric(
-                              vertical: 12.0, horizontal: 15.0),
-                          border: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.grey, width: 2.0),
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.grey, width: 1.0),
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                      ),
                     ),
                   ],
                 ),
@@ -155,54 +229,61 @@ class _informationPageState extends State<informationPage> {
                   height: 1,
                 ),
                 Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 30.0, vertical: 12.0),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0, vertical: 12.0),
 
-                      child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.grey, // สีขอบ
-                          width: 1.0, // ขนาดความหนาขอบ
-                        ),
-                        borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey, // สีขอบ
+                        width: 1.0, // ขนาดความหนาขอบ
                       ),
-                      
-                      child: DropdownButton(
-                        hint: Text("ระดับการออกกำลังกาย",
-                        style: TextStyle(
-                        color: brown, // สีของ hint text
-                        fontSize: 16, // ขนาด font ของ hint text
-                        fontFamily: 'Garuda',
-                        ),
-                        ),
-                        icon: Icon(Icons.arrow_drop_down , color: brown,),
-                        iconSize: 36,
-                        style: TextStyle(
-                                          color: brown,
-                                          fontSize: 16,
-                                          fontFamily: 'Garuda',
-                                        ),
-                        value: valueselect,
-                        onChanged: (newValue) {
-                          setState(() {
-                            valueselect = newValue as String?;
-                          });
-                        },
-                        items: listitem.map((item) {
-                          return DropdownMenuItem(
-                            value: item as String?,
-                            child: Text(item! ,style: TextStyle(
-                                                color: brown, // สีของเนื้อหาใน DropdownMenuItem
-                                                fontSize: 16, // ขนาด font ของเนื้อหาใน DropdownMenuItem
-                                                fontFamily: 'Garuda',
-                                              ),),
-                          );
-                        }).toList(),
-                      ),
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
+                    child: DropdownButton(
+                      hint: Text(
+                        "ระดับการออกกำลังกาย",
+                        style: TextStyle(
+                          color: brown, // สีของ hint text
+                          fontSize: 16, // ขนาด font ของ hint text
+                          fontFamily: 'Garuda',
+                        ),
+                      ),
+                      icon: Icon(
+                        Icons.arrow_drop_down,
+                        color: brown,
+                      ),
+                      iconSize: 36,
+                      style: TextStyle(
+                        color: brown,
+                        fontSize: 16,
+                        fontFamily: 'Garuda',
+                      ),
+                      value: valueselect,
+                      onChanged: (newValue) {
+                        setState(() {
+                          valueselect = newValue as String?;
+                        });
+                      },
+                      items: listitem.map((item) {
+                        return DropdownMenuItem(
+                          value: item as String?,
+                          child: Text(
+                            item!,
+                            style: TextStyle(
+                              color: brown, // สีของเนื้อหาใน DropdownMenuItem
+                              fontSize:
+                                  16, // ขนาด font ของเนื้อหาใน DropdownMenuItem
+                              fontFamily: 'Garuda',
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
 
-                //SizedBox(
-                 // height: 10,
+                  //SizedBox(
+                  // height: 10,
                 ),
                 ElevatedButton(
                   style: buttonlgin,
@@ -216,7 +297,6 @@ class _informationPageState extends State<informationPage> {
                   },
                   child: Text('ยืนยัน', style: TextStyles.Tlogin),
                 ),
-               
                 SizedBox(
                   height: 10,
                 ),
