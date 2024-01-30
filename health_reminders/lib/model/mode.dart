8ec93013a1402e97b6eb9bbee3fb0cbb6ab67094
magomie <113960532+magomie@users.noreply.github.com>
2024-01-30 18:23:16 +0700

    Test
    Co-Authored-By: NEO <75066153+N3O-N@users.noreply.github.com>

--- health_reminders/lib/model/mode.dart
+++ health_reminders/lib/model/mode.dart
@@ -4,11 +4,14 @@ class User {
   final int id;
   final String username;
   final String password;
+  final String Type;
 
-  User({required this.id, required this.username, required this.password});
+  User({required this.id, required this.username, required this.password , required this.Type});
 
   factory User.formJson(Map<String, dynamic> json) {
     return User(
-        id: json['id'], username: json['username'], password: json['password']);
+        id: json['id'], username: json['username'], password: json['password'],Type: json['Type']);
   }
 }
+
+
