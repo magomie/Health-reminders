c006ec68f3112fd814bbe6d8d4a8e2327d017039
NEO <75066153+N3O-N@users.noreply.github.com>
2024-02-14 20:07:16 +0700

    แก้ conn firebase

--- health_reminders/lib/pages/main.dart
+++ health_reminders/lib/pages/main.dart
@@ -2,11 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:health_reminders/pages/lanrelog/home.dart';
 import 'package:health_reminders/pages/lanrelog/landing.dart';
+import 'package:health_reminders/pages/test.dart';
 import 'package:health_reminders/styles/color.dart';
 
 import '../firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
