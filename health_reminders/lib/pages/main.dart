9011f76d5c72e491ed8a0bbff80e99441d2198f1
NEO <75066153+N3O-N@users.noreply.github.com>
2024-02-14 18:56:52 +0700

    firebase conn

--- health_reminders/lib/pages/main.dart
+++ health_reminders/lib/pages/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:health_reminders/pages/lanrelog/home.dart';
 import 'package:health_reminders/pages/lanrelog/landing.dart';
 import 'package:health_reminders/styles/color.dart';
 
-void main() {
+import '../firebase_options.dart';
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
